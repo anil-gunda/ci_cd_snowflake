6dc6f349ce1e8f0a114bf56b25d90aff6cd3eff3
anil-mudhiraj <anil9618454029@gmail.com>
2024-06-21 17:42:23 +0530

    Update changelogfile

--- DEV/CONFIG/TABLES/SAMPLE_TABLE_2.sql
+++ DEV/CONFIG/TABLES/SAMPLE_TABLE_2.sql
@@ -1,2 +1,7 @@
 --liquibase formatted sql
+--changeset author:create_sample_table_1
 CREATE or REPLACE TABLE DEV.CONFIG.SAMPLE_TABLE_1 (ID NUMBER);
+
+--liquibase formatted sql
+--changeset author:rename_id_column
+ALTER TABLE CONFIG.SAMPLE_TABLE_1 RENAME COLUMN ID TO ID_1;
